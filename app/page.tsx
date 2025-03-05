94872eb2bb078ff5b43533f68b817b2a0cfdc64e
Jeffrey Chery <jeffchery009@gmail.com>
2025-03-05 04:04:47 -0500

    Additional Configurations for Tailwind & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,19 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
-      <Image src="/logo.png" alt="NoteFlow.io" width={100} height={100} />
-      <h1 className="text-4xl text-white">Welcome to Noteflow</h1>
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 px-4 py-8 sm:px-6 lg:px-8">
+      <div className="flex flex-col items-center space-y-6">
+        <Image
+          src="/logo.png"
+          alt="NoteFlow.io"
+          width={100}
+          height={100}
+          className="rounded-full shadow-lg transition-transform duration-300 hover:scale-110"
+        />
+        <h1 className="text-center text-4xl font-bold text-white">
+          Welcome to Noteflow
+        </h1>
+      </div>
     </div>
   );
 };
