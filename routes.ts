b03af68c10b8d8b7c154b6069aed622d400f953c
Jeffrey Chery <jeffchery009@gmail.com>
2025-04-01 05:49:44 -0400

    Added Additional Routes

--- routes.ts
+++ routes.ts
@@ -1,7 +1,13 @@
 export const ROUTES = {
   HOME: "/",
+  HOME_PAGE: "/home",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   FOLDERS: "/folders",
   NOTES: "/notes",
+  SHARED_WITH_ME: "/shared-with-me",
+  FAVORITES: "/favorites",
+  TRASH: "/trash",
+  PROFILE: "/profile",
+  SETTINGS: "/settings",
 };
