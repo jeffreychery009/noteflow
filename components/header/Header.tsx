cf98cc2eb9a58eb12e163dcdc6af6b8a418060a4
Jeffrey Chery <jeffchery009@gmail.com>
2025-04-04 14:22:40 -0400

    Designed and Configured the Header Component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,21 +1,35 @@
+import { Settings } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
+import { auth } from "@/auth";
+import { ROUTES } from "@/routes";
+
+import ProfileImage from "./ProfileImage";
 import Title from "./Title";
 import WelcomeMessage from "./WelcomeMessage";
 import MobileNavigation from "../navigation/MobileNavigation";
 import { ThemeToggle } from "../toggles/theme-toggle";
+import { Button } from "../ui/button";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
   return (
     <header className="sticky top-0 z-50 flex justify-between border-b border-gray-200 bg-white px-4 dark:border-gray-800 dark:bg-gray-950">
-      <div className="flex h-16 items-center gap-4">
+      <div className="flex h-16 items-center">
         <Title />
         <WelcomeMessage />
       </div>
       <div className="flex h-16 items-center">
         <div className="flex items-center gap-4">
           <ThemeToggle />
-          <div className="flex items-center md:hidden">
+          <Link href={ROUTES.SETTINGS}>
+            <Button variant="ghost" className="flex items-center gap-2">
+              <Settings />
+            </Button>
+          </Link>
+          <ProfileImage />
+          <div className="md:hidden">
             <MobileNavigation />
           </div>
         </div>
