a3ce2b76aba0f22aaf8e783ce7093aceb171dab6
Jeffrey Chery <jeffchery009@gmail.com>
2025-04-28 03:27:35 -0400

    Defined a file for Types. Configured Folder, Folder Data and a reusable Fetching function

---
+++ hooks/folders/types.ts
@@ -0,0 +1,22 @@
+export type Folder = {
+  _id: string;
+  title: string;
+  itemCount: number;
+  createdAt: string;
+  updatedAt?: string;
+};
+
+export interface FolderData {
+  success: boolean;
+  data: {
+    folders: Folder[];
+  };
+}
+
+export const folderFetcher = (url: string) =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      if (!json.success) throw new Error(json.message || "API Error");
+      return json as FolderData;
+    });
