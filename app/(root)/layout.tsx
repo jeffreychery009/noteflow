e96494e566d80ea389949429740484e654728f0f
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-27 05:00:13 -0400

    Changed the layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from "@/components/sidebar/Sidebar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-black">
-      <div className="hidden border-r border-gray-200 md:block md:w-64 dark:border-gray-800">
+      <div className="hidden border-r border-gray-200 md:block md:w-80 dark:border-gray-800">
         <Sidebar />
       </div>
       <div className="flex flex-1 flex-col overflow-hidden">
