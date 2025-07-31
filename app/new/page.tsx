365e8bed1c0d40138137bf8650887d77cc6385ea
Jeffrey Chery <jeffchery009@gmail.com>
2025-07-31 03:15:43 -0400

    Removed restraints of Rich text editor

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 
 import RichTextEditor from "@/components/rich-text-editor";
+
 const page = () => {
-  return (
-    <div className="mx-auto max-w-3xl p-8">
-      <RichTextEditor />
-    </div>
-  );
+  return <RichTextEditor />;
 };
 
 export default page;
